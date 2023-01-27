ca1ba3e5f4b47d4dfee14dba90259a7efda758f5
Ruslana Solovova <ruslanasolovova@gmail.com>
2023-01-27 15:41:59 -0800

    Password secured

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,15 +1,20 @@
 import streamlit as st
 from send_email import send_email
+import pandas
 
-st.header("Contact Us")
+df = pandas.read_csv("topics.csv")
 
 with st.form(key="email_form"):
      user_email = st.text_input("Your email address")
+     option = st.selectbox(
+     'What topic do you want to discuss?',
+     df["topic"])
      raw_message = st.text_area("Your message")
      message = f"""\
 Subject: New email from {user_email}
 
 From: {user_email}
+Topic {option}
 {raw_message}
 """
      button = st.form_submit_button("Submit")
--- send_email.py
+++ send_email.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
@@ -6,7 +7,7 @@ def send_email(message):
     port = 465
 
     username = "neoveta@gmail.com"
-    password = "cewswxecyskdaanc"
+    password = os.getenv("PASSWORD")
 
     receiver = "neoveta@gmail.com"
     context = ssl.create_default_context()
