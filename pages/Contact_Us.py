460f7f24132ec1392b101fbdc0a79f26ec9f633c
Ruslana Solovova <ruslanasolovova@gmail.com>
2023-01-27 14:20:50 -0800

    Added Contact Us page

---
+++ pages/Contact_Us.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.header("Contact Us")
+
+with st.form(key="email_form"):
+     user_email = st.text_input("Your email address")
+     message = st.text_area("Your message")
+     button = st.form_submit_button("Submit")
+     if button:
+         message = message + user_email
+
+
+
